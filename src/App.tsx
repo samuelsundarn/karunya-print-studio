eb8821372a73f83b4fb40952e9ea3c921a965522
Revanth Javvadi <javvadirevanth2523@gmail.com>
2025-09-19 19:16:08 +0530

    Configure for GitHub Pages deployment - Add deployment scripts, .nojekyll file, and update configuration

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.PROD ? "/karunya-printers" : ""}>
         <AppContent />
       </BrowserRouter>
     </TooltipProvider>
