8283c58b9d1cc9fac265e5b060434955a60cbc5b
Revanth Javvadi <javvadirevanth2523@gmail.com>
2025-09-19 19:06:53 +0530

    fix: set BrowserRouter basename to BASE_URL for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <AppContent />
       </BrowserRouter>
     </TooltipProvider>
